85d3f5210b9f84bb1a2e7d28684538a9ccd38e34
hemant <hemantsonu20@gmail.com>
2017-01-29 01:38:11 +0530

    fixes

--- src/test/java/com/github/spring/jwt/ThreadServiceTest.java
+++ src/test/java/com/github/spring/jwt/ThreadServiceTest.java
@@ -16,7 +16,9 @@
  */
 package com.github.spring.jwt;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.concurrent.ExecutionException;
 
